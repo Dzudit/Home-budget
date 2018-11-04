f8c5c760b58af237479b09eecbe24007076d5af6
Judyta Wis <judyta.wis@eset.pl>
2018-11-04 20:44:59 +0100

    first trail with database

---
+++ backend/api/models.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here. create database here
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    password = models.CharField(max_length=20)
+    email = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name + ' - ' + self.password + ' - ' + self.email
+
+class Field(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
