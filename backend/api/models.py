9707409cef966954963a9fdf47fef6e2263b1200
Judyta Wis <judyta.wis@eset.pl>
2018-11-15 15:18:04 +0100

    created log in

--- backend/api/models.py
+++ backend/api/models.py
@@ -15,3 +15,7 @@ class User(models.Model):
 
 class Field(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    field_name = models.CharField(max_length=60, default='SOME STRING')
+
+    def __str__(self):
+        return self.field_name
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,27 @@
 from django.http import HttpResponse
+from django.shortcuts import  render
+from .models import User
+from django.http import Http404
+
 
 def index(request):
-    return HttpResponse("<h1>Hellow</h1>")
+    all_Users = User.objects.all()
+   # template = loader.get_template('login.html')
+
+    context = {
+        'all_Users' : all_Users,
+    }
+   # html = ''
+    #for user in all_Users:
+     #   url = '/api/' + str(User.id) + '/'
+      #  html += '<a href="'+ url +'">'+ user.name+'</a><br>'
+    #return HttpResponse(html)
+    #return HttpResponse(template.render(context, request))
+    return render(request, 'home.html', context)
+
+def detail(request, user_id):
+    try:
+        user = User.objects.get(pk=user_id)
+    except User.DoesNotExist:
+        raise Http404("user does not exist")
+    return render(request, 'detail.html', {'user': user})
